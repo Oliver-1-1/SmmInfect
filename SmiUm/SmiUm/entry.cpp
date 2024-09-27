bae89d4e35304e3a4aab8e0a637a8104e23600e6
Oliver <66334947+Oliver-1-1@users.noreply.github.com>
2024-09-27 21:20:00 +0200

    Update entry.cpp

--- SmiUm/SmiUm/entry.cpp
+++ SmiUm/SmiUm/entry.cpp
@@ -19,7 +19,7 @@ extern "C"
 #pragma pack(1) // Ensure that alignment is 1-byte
 typedef struct _SmmCommunicationProtocol
 {
-  UINT8 magic = 'i';
+  UINT8 magic = 'i'; // Magic identifier 
   UINT8 process_name[30] = { 0 };
   UINT16 module_name[30] = { 0 };
   UINT64 offset = 0;
@@ -30,21 +30,25 @@ typedef struct _SmmCommunicationProtocol
 } SmmCommunicationProtocol, * PSmmCommunicationProtocol;
 #pragma pack() // reset
 
+// Allocate a section so the SMM driver knows what section the communication payload is in.
 #pragma section(".ZEPTA", read, write)
 __declspec(allocate(".ZEPTA")) volatile SmmCommunicationProtocol protocol;
 
 void TriggerSmi()
 {
   BOOLEAN e = false;
-  const NTSTATUS status = RtlAdjustPrivilege(22L, true, false, &e);
+  //Get the right privileges to call NtSetSystemEnvironmentValueEx. ( SeSystemEnvironmentPrivilege )
+  const NTSTATUS status = RtlAdjustPrivilege(22, true, false, &e);
   
   if (!NT_SUCCESS(status))
   {
+    // We need admin privileges!
     printf("No suitable permission! Open as admin!\n");
     return;
   }
 
   GUID guid = { 0 };
+  // Try to get a variable that doesn't exist so we don't trigger a runtime cache hit
   UNICODE_STRING name = RTL_CONSTANT_STRING(L"ZeptaVar");
   char buffer[8];
   NtSetSystemEnvironmentValueEx(&name, &guid, buffer, sizeof(buffer), EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS);
@@ -57,21 +61,24 @@ void main()
 
   while (true)
   {
+    //Read the first 15 bytes of explorer.exe. This will include DOS header if the SMM module is setup correctly.
     strcpy((char*)protocol.process_name, "explorer.exe");
     wcscpy((wchar_t*)protocol.module_name, L"explorer.exe");
     protocol.offset = 0;//= 0x2F000;
     protocol.read_size = 15;
     memset((void*)protocol.read_buffer, 0, sizeof(protocol.read_buffer));
+
+    // Trigger a SMI and the driver will find this process.
     TriggerSmi();
 
-
+    // Print out the bytes the SMM driver read for us.
     printf("Smi count: %llu\n", protocol.smi_count);
 
     for (int i = 0; i < protocol.read_size; i++)
     {
       printf("%02X ", protocol.read_buffer[i]);
     }
-
+    
     Sleep(50);
   }
 }
