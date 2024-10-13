2e8fe2f677bfbf87424b3a41f3573853d4c7723d
Oliver <66334947+Oliver-1-1@users.noreply.github.com>
2024-10-13 15:11:45 +0200

    Update memory.h

--- SmmInfect/memory.h
+++ SmmInfect/memory.h
@@ -19,21 +19,22 @@
 #include <IndustryStandard/PeImage.h>
 #include <Guid/GlobalVariable.h>
 
-EFI_RUNTIME_SERVICES* gRT;
-EFI_BOOT_SERVICES* gBS;
-EFI_SYSTEM_TABLE* gST;
+EFI_RUNTIME_SERVICES *gRT;
+EFI_BOOT_SERVICES *gBS;
+EFI_SYSTEM_TABLE *gST;
 
 #define Windows
 // #define Linux UNSUPPORTED
-EFI_SMM_SYSTEM_TABLE2* gSmst2;
+EFI_SMM_SYSTEM_TABLE2 *gSmst2;
 
-void* ZMemSet(void* ptr, int value, UINT64 num);
+void *ZMemSet(void *ptr, int value, UINT64 num);
+EFI_STATUS SetupMemoryMap();
 BOOLEAN IsAddressValid(UINT64 address);
 
 //
 // Physical memory
 //
-UINT8* ReadPhysical(UINT64 address, UINT8* buffer, UINT64 length);
+UINT8 *ReadPhysical(UINT64 address, UINT8 *buffer, UINT64 length);
 UINT8 ReadPhysical8(UINT64 address);
 UINT16 ReadPhysical16(UINT64 address);
 UINT32 ReadPhysical32(UINT64 address);
@@ -42,7 +43,7 @@ UINT64 ReadPhysical64(UINT64 address);
 //
 // Virtual memory
 //
-UINT8* ReadVirtual(UINT64 address, UINT64 cr3, UINT8* buffer, UINT64 length);
+UINT8 *ReadVirtual(UINT64 address, UINT64 cr3, UINT8 *buffer, UINT64 length);
 UINT8 ReadVirtual8(UINT64 address, UINT64 cr3);
 UINT16 ReadVirtual16(UINT64 address, UINT64 cr3);
 UINT32 ReadVirtual32(UINT64 address, UINT64 cr3);
@@ -53,9 +54,8 @@ UINT64 ReadVirtual64(UINT64 address, UINT64 cr3);
 //
 #ifdef Windows
 UINT64 TranslateVirtualToPhysical(UINT64 cr3, UINT64 address);
-EFI_STATUS MemGetKernelBase(UINT64* base);
-EFI_STATUS MemGetKernelCr3(UINT64* cr3);
-EFI_STATUS SetupMemoryMap();
+EFI_STATUS MemGetKernelBase(UINT64 *base);
+EFI_STATUS MemGetKernelCr3(UINT64 *cr3);
 #endif
 
 //
