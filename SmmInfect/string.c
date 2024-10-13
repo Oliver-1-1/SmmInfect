ec785180b0377f5b1e0f9763980b40017b10b1c9
Oliver <66334947+Oliver-1-1@users.noreply.github.com>
2024-10-13 19:57:25 +0200

    Update string.c

--- SmmInfect/string.c
+++ SmmInfect/string.c
@@ -2,25 +2,25 @@
 
 int ToLower(int c)
 {
-	return (c >= 'A' && c <= 'Z') ? (c + 'a' - 'A') : c;
+  return (c >= 'A' && c <= 'Z') ? (c + 'a' - 'A') : c;
 }
 
 int StrCmpi(const char* str1, const char* str2)
 {
-	while (*str1 && (ToLower(*str1) == ToLower(*str2)))
-	{
-		str1++;
-		str2++;
-	}
-	return *(const unsigned char*)str1 - *(const unsigned char*)str2;
+  while (*str1 && (ToLower(*str1) == ToLower(*str2)))
+  {
+    str1++;
+    str2++;
+  }
+  return *(const unsigned char*)str1 - *(const unsigned char*)str2;
 }
 
 int WcsCmpi(const unsigned short* str1, const unsigned short* str2)
 {
-	while (*str1 && (ToLower(*str1) == ToLower(*str2)))
-	{
-		str1++;
-		str2++;
-	}
-	return *(const unsigned short*)str1 - *(const unsigned short*)str2;
+  while (*str1 && (ToLower(*str1) == ToLower(*str2)))
+  {
+    str1++;
+    str2++;
+  }
+  return *(const unsigned short*)str1 - *(const unsigned short*)str2;
 }
